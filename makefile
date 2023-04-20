a64e8dbe9ef786c894fcf894299f7d8d968a817e
Daniel Yang <daniely@silber-courses.cis.udel.edu>
2023-04-19 20:59:07 -0400

    added and finished pthread and openmp files

--- makefile
+++ makefile
@@ -3,3 +3,7 @@ image:image.c image.h
 	gcc -g image.c -o image -lm
 clean:
 	rm -f image output.png
+pthread: pthread.c pthread.h
+	gcc -g pthread.c -o pthread -lm -lpthread
+openmp: openmp.c image.h
+	gcc -c openmp.c -o openmp -lm -fopenmp
---
+++ pthread.h
@@ -0,0 +1,31 @@
+#ifndef ___IMAGE
+#define ___IMAGE
+#include <stdint.h>
+
+#define Index(x,y,width,bit,bpp) y*width*bpp+bpp*x+bit
+
+typedef struct{
+    uint8_t* data;
+    int width;
+    int height;
+    int bpp;
+} Image;
+
+enum KernelTypes{EDGE=0,SHARPEN=1,BLUR=2,GAUSE_BLUR=3,EMBOSS=4,IDENTITY=5};
+
+typedef double Matrix[3][3];
+
+struct args_struct {
+    Image* original;
+    Image* edit;
+    enum KernelTypes type;
+    long rank;
+    int count;
+};
+
+uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm);
+void *convolute(void* vals);
+int Usage();
+enum KernelTypes GetKernelType(char* type);
+
+#endif
